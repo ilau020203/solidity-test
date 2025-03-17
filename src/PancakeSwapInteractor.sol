8b46f7f8d907c93f50f5d0af14fa05358468b82b
ilau020203 <antip19052016@gmail.com>
2025-03-17 23:09:46 +0300

    init

---
+++ src/PancakeSwapInteractor.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+import "./interfaces/IPancakeFactory.sol";
+import "./interfaces/IPancakePair.sol";
+import "./interfaces/IPancakeRouter.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract PancakeSwapInteractor {
+    IPancakeFactory public immutable factory;
+    IPancakeRouter public immutable router;
+    address public immutable WETH;
+
+    constructor(address _factory, address _router) {
+        factory = IPancakeFactory(_factory);
+        router = IPancakeRouter(_router);
+        WETH = router.WETH();
+    }
+
+    receive() external payable {}
+
+
+}
